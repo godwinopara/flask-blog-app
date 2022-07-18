8bc8fad4c152ed2c82f682cfe9ce5fdfe4ccbafd
godwin opara <godwinopara62@yahoo.com>
2022-07-19 00:22:55 +0100

    installed flask-login and used flask-login to handle user login and user loggout

--- app/__init__.py
+++ app/__init__.py
@@ -1,21 +1,25 @@
+from cmath import log
 from flask import Flask, flash, redirect, render_template, url_for
 from config import Config
 from app.forms import LoginForm
 from flask_migrate import Migrate
-from flask_sqlalchemy import SQLAlchemy
-from app.models import db
-
+from app.models import db, User
+from flask_login import LoginManager, current_user, login_user
 
 # from app import routes
+
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
-    db.init_app(app)
+
+    with app.app_context():
+        db.init_app(app)
     return app
 
 
 app = create_app()
+login = LoginManager(app)
 migrate = Migrate(app, db)
 
 
@@ -40,8 +44,20 @@ def index():
 def login():
     form = LoginForm()
 
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+
     if form.validate_on_submit():
-        flash(
-            f"Login request for user {form.username.data}, remember_me={form.remember_me.data}")
+
+        user = User.query.filter_by(username=form.username.data).first()
+
+        if user is None or not user.check_password(form.password.data):
+            flash("Invalid Username Or Password")
+            return redirect(url_for('login'))
+
+        login_user(user, remember=form.remember_me.data)
+
+        flash("Login Successfull")
         return redirect(url_for("index"))
+
     return render_template("login.html", title="Sign in", form=form)
--- app/models.py
+++ app/models.py
@@ -1,11 +1,20 @@
 from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import ForeignKey
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from app import login
+
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
 
 db = SQLAlchemy()
 
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     __tablename__ = "user"
 
     id = db.Column(db.Integer, primary_key=True)
@@ -13,6 +22,12 @@ class User(db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     password = db.Column(db.String(128))
     posts = db.relationship("Post", backref="author", lazy="dynamic")
+
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
 
     def __repr__(self) -> str:
         return f"<User {self.username}>"
