eced0ab8b86e14d5febfea8cbaa7a8d634063c54
godwin opara <godwinopara62@yahoo.com>
2022-07-18 18:48:37 +0100

    added a new Post model with relationship to the User model

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
-from enum import unique
+from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import ForeignKey
 
 db = SQLAlchemy()
 
@@ -11,6 +12,19 @@ class User(db.Model):
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password = db.Column(db.String(128))
+    posts = db.relationship("Post", backref="author", lazy="dynamic")
 
     def __repr__(self) -> str:
         return f"<User {self.username}>"
+
+
+class Post(db.Model):
+    __tablename__ = "post"
+
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+
+    def __repr__(self) -> str:
+        return f"<Post {self.body}>"
